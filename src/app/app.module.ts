cb63a74e2f3e1598b80beaee5ec3787d4a62e04e
Deepak Yadav <dy959485@gmail.com>
2025-11-07 19:38:47 +0530

    styling change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,14 +21,25 @@ import { CustomerDetailComponent } from './modules/customer-detail/customer-deta
 import { EditProductInfoComponent } from './modules/edit-product-info/edit-product-info.component';
 import { NotificationsComponent } from './modules/notifications/notifications.component';
 
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAdoQAurH1lK2IMOqVdBZvWNin0uRmSLsY",
+//   authDomain: "kent-3b295.firebaseapp.com",
+//   projectId: "kent-3b295",
+//   storageBucket: "kent-3b295.firebasestorage.app",
+//   messagingSenderId: "1053046070991",
+//   appId: "1:1053046070991:web:40ff295f28bd90072ce1ba",
+//   measurementId: "G-CZPZH8HW98"
+// };
+
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAdoQAurH1lK2IMOqVdBZvWNin0uRmSLsY",
-  authDomain: "kent-3b295.firebaseapp.com",
-  projectId: "kent-3b295",
-  storageBucket: "kent-3b295.firebasestorage.app",
-  messagingSenderId: "1053046070991",
-  appId: "1:1053046070991:web:40ff295f28bd90072ce1ba",
-  measurementId: "G-CZPZH8HW98"
+  apiKey: "AIzaSyDh--ZV33Pjg1_PjvQbi6LNGzJB6uql5h4",
+  authDomain: "customer-management-276c3.firebaseapp.com",
+  projectId: "customer-management-276c3",
+  storageBucket: "customer-management-276c3.firebasestorage.app",
+  messagingSenderId: "445035486831",
+  appId: "1:445035486831:web:1188525e09a19c0fcc779a",
+  measurementId: "G-ST6LLSK6HC"
 };
 
 @NgModule({
